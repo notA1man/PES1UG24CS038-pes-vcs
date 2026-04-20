c10d91aafde57eb75d61b8c0856dda7493243918
notA1man <nota1mnawab@gmail.com>
2026-04-20 14:40:34 +0530

    Phase 4: populate commit metadata fields

--- commit.c
+++ commit.c
@@ -211,5 +211,9 @@ int commit_create(const char *message, ObjectID *commit_id_out) {
         c.has_parent = 0;
     }
 
+    snprintf(c.author, sizeof(c.author), "%s", pes_author());
+    c.timestamp = (uint64_t)time(NULL);
+    snprintf(c.message, sizeof(c.message), "%s", message);
+
     return -1;
 }
